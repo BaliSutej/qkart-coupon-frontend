e0bcae08fe4ae2bbfc19b26bf25b07eb9a08a26b
Sutej bali <sutejbali@gmail.com>
2022-01-16 16:28:49 +0530

    Updated applycoupon component

--- src/components/ApplyCoupon.js
+++ src/components/ApplyCoupon.js
@@ -4,6 +4,54 @@ import './ApplyCoupon.css';
 
 class ApplyCoupon extends react.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            called : true,
+            amount : 0,
+            couponCode : "",
+            res :"",
+            message:""
+        }
+    }
+
+
+amountChangeHandler = (e) => {
+    this.setState({
+        "amount": e.target.value
+    })
+}
+
+couponCodeChangeHandler = (e) => {
+    this.setState({
+        "couponCode": e.target.value
+    })
+}
+
+
+getDiscount = async (amount,couponCode) => {
+    let res = await fetch(`https://qkart-coupon-backend.herokuapp.com/get-discount?couponcode=${couponCode}&amount=${amount}`);
+    res = await res.json();
+    console.log(res);
+    if(res.error){
+        this.setState({
+            "res":res.error
+        })
+    }else{
+        this.setState({
+            "res": `Discount of ${res.discount_amount} can be applied`,
+            "message":res.message
+        })
+    }
+
+}
+
+
+btnClickHandler = (e) => {
+    console.log(this.state.amount);
+    console.log(this.state.couponCode);
+    this.getDiscount(this.state.amount,this.state.couponCode);
+}
 
     render() {
         return (
@@ -11,11 +59,12 @@ class ApplyCoupon extends react.Component {
                 <div id='applycoupon-cmpt'>
                     <div className="cartAmount">
                         <div>Enter cart amount </div>
-                        <input type="text"></input>
+                        <input onChange={this.amountChangeHandler} value={this.state.amount} type="text"></input>
                     </div>
                     <div className='cartCoupon' >
-                        <input placeholder='Enter Coupon Code' type="text"></input>
-                        <button>Apply</button>
+                        <input onChange={this.couponCodeChangeHandler} value={this.state.couponCode} placeholder='Enter Coupon Code' type="text"></input>
+                        <button onClick={this.btnClickHandler} >Apply</button>
+                        {(this.state.called ?<> <p>{this.state.res} </p> <p>{this.state.message}</p></>: null)}
                     </div>
                 </div>
             </>
